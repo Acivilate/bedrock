57df71d0ee856fe1f5deace36adba51629a80622
John Crisp <jcrisp@acivilate.com>
2025-04-28 13:20:47 -0400

    initial amplify setup

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,31 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifybedrock23fca3c2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "bedrockcognito123GroupRole": "string"
+    }
+  },
+  "function": {
+    "S3Trigger581a0d68": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "chatS3Bucket": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
